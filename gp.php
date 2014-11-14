250ef2cd9757c148305f35574ade56a3c252567e
Leroi Floriano de Oliveira <leroioliveira@ifsul.edu.br>
2014-11-14 11:14:52 -0200

    ajustes para que o fonte fique mais organizado

--- gp.php
+++ gp.php
@@ -1,24 +1,25 @@
 <?php
-
+global $dicionario;
 $quebraLinha = "\n";
 $fimLinha = ';';
 
 $dicionario  = array(
-		array(
-				"estruturas",
+		"estruturas"=>array(
+				"function FUN(VARS)",
 				"if(COND)",
 				"else",
 				"else if(COND)",
 				"foreach(VAR as VAR_NIVEL)"
 				),
-		array(
-				"funcoes",
+		"retorno"=>array(
+				"return VAR"
+				),
+		"funcoes"=>array(
 				"rand()",
 				"md5(VAR)",
 				"break"
 				),
-		array(
-				"operadores_comparacao",
+		"operadores_comparacao"=>array(
 				">",
 				"<",
 				">=",
@@ -28,13 +29,11 @@ $dicionario  = array(
 				"===",
 				"!=="
 				),
-		array(
-				"operadores_logicos",
+		"operadores_logicos"=>array(
 				"&&",
 				"||"
 				),
-		array(
-				"operadores_matematicos",
+		"operadores_matematicos"=>array(
 				"+",
 				"-",
 				"/",
@@ -42,13 +41,7 @@ $dicionario  = array(
 				".",
 				"&",
 				"^",
-				"|",
-				"=",
-				"+=",
-				".=",
-				"-=",
-				"*=",
-				"/="
+				"|"
 				)
 	);
 $pos_estruturas = 0;
@@ -57,38 +50,52 @@ $pos_estruturas = 0;
 $nIndividuos = 2;
 $interacoes = 10;
 
+$tabulacao = "    ";
 
 for($i = 0; $i <$nIndividuos; $i++){
-	$profundidade = 0;
-	$output = "<?php".$quebraLinha;
-	$variaveis = array();
+    $profundidade = 0;
+    $output = "<?php".$quebraLinha;
+    $variaveis = array();
 
-	for($i2 = 0; $i2<$interacoes; $i2++){	
+    for($i2 = 0; $i2<$interacoes; $i2++){
+        $profundidade_nova = $profundidade;
+        if($i2!=0){
+            $profundidade_nova = rand(-1,1)+$profundidade;
+            $profundidade_nova = ($profundidade_nova)<0?0:$profundidade_nova;
+        }
 
-		$profundidade_nova = ($profundidade_nova = rand(-1,1)+$profundidade)<0?0:$profundidade_nova;
+        if($profundidade_nova>$profundidade){
+            $p1 = $pos_estruturas;
+            $p2 = rand(1, (count($dicionario[$p1])-1));
+            //$output .= str_repeat($tabulacao, $profundidade_nova);
+            $output .= $dicionario[$p1][$p2] ."{".$quebraLinha ;			
+        }else if($profundidade_nova<$profundidade){
+                
+            $p1 = rand(1, (count($dicionario)-1));
+            $p2 = rand(1, (count($dicionario[$p1])-1));
 
-		if($profundidade_nova>$profundidade){
-			$p1 = $pos_estruturas;
-			$p2 = rand(1, (count($dicionario[$p1])-1));
-			$output .= str_repeat("	", $profundidade_nova);
-			$output .= $dicionario[$p1][$p2] ."{".$quebraLinha ;			
-		}else if($profundidade_nova<$profundidade){
-			$p1 = rand(0, (count($dicionario)-1));
-			$p2 = rand(1, (count($dicionario[$p1])-1));
-			$output .= str_repeat("	", $profundidade)."}".$quebraLinha ;
-			$output .= str_repeat("	", $profundidade_nova);
-			$output .= $dicionario[$p1][$p2] . $fimLinha.$quebraLinha ;
-		}else{
-			$p1 = rand(0, (count($dicionario)-1));
-			$p2 = rand(1, (count($dicionario[$p1])-1));
-			$output .= str_repeat("	", $profundidade_nova);
-			$output .= $dicionario[$p1][$p2] . $fimLinha.$quebraLinha ;
-		}
-		
+            $output .= str_repeat($tabulacao, $profundidade);
+            $output .= $dicionario[$p1][$p2] . $fimLinha.$quebraLinha ;
 
-		$profundidade = $profundidade_nova;
-	}
-	file_put_contents("populacao/$i.php", $output);
+            $output .= str_repeat($tabulacao, $profundidade_nova)."}".$quebraLinha ;
+                
+        }else{
+                $p1 = rand(1, (count($dicionario)-1));
+                $p2 = rand(1, (count($dicionario[$p1])-1));
+                $output .= str_repeat($tabulacao, $profundidade_nova);
+                $output .= $dicionario[$p1][$p2] . $fimLinha.$quebraLinha ;
+        }
+
+
+            $profundidade = $profundidade_nova;
+    }
+    if($profundidade >0){
+        while($profundidade>=0){
+            $output .= str_repeat($tabulacao, $profundidade)."}".$quebraLinha ;
+            $profundidade--;
+        }
+    }
+    file_put_contents("populacao/$i.php", $output);
 }
 
 
