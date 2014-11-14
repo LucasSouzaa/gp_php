650929c374a6fb3ef4abc4352030061c651fb540
Leroi Floriano de Oliveira <leroioliveira@ifsul.edu.br>
2014-11-14 14:19:22 -0200

    tentando colocar a organização em funções para poder estruturar primeiro e depois gerar as possibilidades.

--- gp.php
+++ gp.php
@@ -1,23 +1,22 @@
 <?php
-global $dicionario;
-$quebraLinha = "\n";
-$fimLinha = ';';
+define("QUEBRA_LINHA" ,"<br>");
+define("FIM_LINHA" ,";");
+define("TABULACAO" ,"    ");
 
 $dicionario  = array(
 		"estruturas"=>array(
-				"function FUN(VARS)",
-				"if(COND)",
-				"else",
-				"else if(COND)",
-				"foreach(VAR as VAR_NIVEL)"
-				),
-		"retorno"=>array(
-				"return VAR"
+				array( "codigo"=>"if(COND)",
+                                    "subcodigo"=>array("else",
+				"else if(COND)")
+                                    ),
+				array(
+                                    "codigo"=>"foreach(VAR as VAR_NIVEL)",
+                                    "funcoes"=>array("break")
+                                    )
 				),
 		"funcoes"=>array(
 				"rand()",
-				"md5(VAR)",
-				"break"
+				"md5(VAR)"
 				),
 		"operadores_comparacao"=>array(
 				">",
@@ -50,11 +49,11 @@ $pos_estruturas = 0;
 $nIndividuos = 2;
 $interacoes = 10;
 
-$tabulacao = "    ";
 
+/*
 for($i = 0; $i <$nIndividuos; $i++){
     $profundidade = 0;
-    $output = "<?php".$quebraLinha;
+    $output = "<?php".QUEBRA_LINHA;
     $variaveis = array();
 
     for($i2 = 0; $i2<$interacoes; $i2++){
@@ -67,23 +66,23 @@ for($i = 0; $i <$nIndividuos; $i++){
         if($profundidade_nova>$profundidade){
             $p1 = $pos_estruturas;
             $p2 = rand(1, (count($dicionario[$p1])-1));
-            //$output .= str_repeat($tabulacao, $profundidade_nova);
-            $output .= $dicionario[$p1][$p2] ."{".$quebraLinha ;			
+            //$output .= str_repeat(TABULACAO, $profundidade_nova);
+            $output .= $dicionario[$p1][$p2] ."{".QUEBRA_LINHA ;			
         }else if($profundidade_nova<$profundidade){
                 
             $p1 = rand(1, (count($dicionario)-1));
             $p2 = rand(1, (count($dicionario[$p1])-1));
 
-            $output .= str_repeat($tabulacao, $profundidade);
-            $output .= $dicionario[$p1][$p2] . $fimLinha.$quebraLinha ;
+            $output .= str_repeat(TABULACAO, $profundidade);
+            $output .= $dicionario[$p1][$p2] . FIM_LINHA.QUEBRA_LINHA ;
 
-            $output .= str_repeat($tabulacao, $profundidade_nova)."}".$quebraLinha ;
+            $output .= str_repeat(TABULACAO, $profundidade_nova)."}".QUEBRA_LINHA ;
                 
         }else{
                 $p1 = rand(1, (count($dicionario)-1));
                 $p2 = rand(1, (count($dicionario[$p1])-1));
-                $output .= str_repeat($tabulacao, $profundidade_nova);
-                $output .= $dicionario[$p1][$p2] . $fimLinha.$quebraLinha ;
+                $output .= str_repeat(TABULACAO, $profundidade_nova);
+                $output .= $dicionario[$p1][$p2] . FIM_LINHA.QUEBRA_LINHA ;
         }
 
 
@@ -91,22 +90,56 @@ for($i = 0; $i <$nIndividuos; $i++){
     }
     if($profundidade >0){
         while($profundidade>=0){
-            $output .= str_repeat($tabulacao, $profundidade)."}".$quebraLinha ;
+            $output .= str_repeat(TABULACAO, $profundidade)."}".QUEBRA_LINHA ;
             $profundidade--;
         }
     }
     file_put_contents("populacao/$i.php", $output);
 }
 
+*/
+
+function retornaEstruturaAleatoria($dicionario, $interacoes = 60){
+    $output = "";
+    $profundidade = 0;
+    
+    $output .= "[PRE_NIVEL_$profundidade]".QUEBRA_LINHA;
+    
+    for($i = 0; $i <$interacoes; $i++){
+        $profundidade_nova = $profundidade;
+        if($i>0){
+            $profundidade_nova = rand(-1,1)+$profundidade;
+            $profundidade_nova = ($profundidade_nova)<0?0:$profundidade_nova;
+        }
+        
+        if($profundidade_nova>$profundidade){
+            $output .= "[PRE_INTERACAO_$i]".QUEBRA_LINHA;
+            $output .= $dicionario["estruturas"]
+                            [rand(0, (count($dicionario["estruturas"])-1))]["codigo"] 
+                    ." { //".$profundidade.QUEBRA_LINHA ;
+            
+            
+        }else if($profundidade_nova<$profundidade){
+            $output .= str_repeat(TABULACAO, $profundidade_nova)."}".QUEBRA_LINHA ;    
+        }
+        $profundidade = $profundidade_nova;
+       //$output .= "/*POS_INTERACAO_$i*/".QUEBRA_LINHA;
+    }
+    
+    if($profundidade >0){
+        while($profundidade>0){
+           $output .= str_repeat(TABULACAO, $profundidade)."}".QUEBRA_LINHA ;
+            $profundidade--;
+            $output .= "[POS_NIVEL_$profundidade]".QUEBRA_LINHA;
+        }
+    }
+    $output .= "[POS_NIVEL_$profundidade]".QUEBRA_LINHA;
+    
+    return $output;
+    
+}
+
+echo retornaEstruturaAleatoria($dicionario);
 
 
-
-
-
-
-
-
-
-
-
-
+echo "</code></pre>";
